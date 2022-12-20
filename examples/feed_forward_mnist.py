7611255672cbd7a5eeb6ded7801bbb7ac00a4e7f
Braden Lockwood <bradenlock83@gmail.com>
2022-12-20 10:29:42 -0600

    Vectorizing everything, may remove neuron class

--- examples/feed_forward_mnist.py
+++ examples/feed_forward_mnist.py
@@ -1,7 +1,10 @@
 import sys
+import numpy as np
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parents[1]))
 from slim_grad.parts.linear import Linear
+from slim_grad.parts.neuron import Neuron
+from slim_grad.engine.tensor import Tensor
 
 class FeedForward:
     def __init__(self) -> None:
@@ -11,12 +14,33 @@ class FeedForward:
         self.l4=Linear(128, 64)
         self.l5=Linear(64, 1)
 
+    def forward(self, x):
+        x1=self.l1(x)
+        print(len(x1))
+
     
 
-
+#TODO: Make everything a custom tensor array
 def main():
-    nn=FeedForward()
-    print(nn.l1)
+    # x=np.array([1, 2, 3, 4])
+    # nn=FeedForward()
+    # nn.forward(x)
+    x=Tensor([
+        [2.0, 3.0, -1.0],
+        [3.0, -1.0, 0.5],
+        [0.5, 1.0, 1.0],
+        [1.0, 1.0, -1.0],
+    ])
+    print(x.shape())
+    weights=Tensor.weight_matrix([3,4])
+    print(weights.shape())
+    # y=[1.0, -1.0, -1.0, 1.0]
+    # linear_1=Linear(3, 6)
+    # out=linear_1(x)
+    # print(out, type(out))
+
+
+
 
 
 if __name__ == '__main__':
