52c4a39eb002cdb637981d97db684d184c90d372
Braden Lockwood <bradenlock83@gmail.com>
2023-01-30 07:17:56 -0600

    created tensor class

---
+++ nano_grad/engine/tensor.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+class Tensor:
+    def __init__(self, data, _children=(), _op='', label='') -> None:
+        self.data=data
+        self._prev=set(_children)
+        self._op=_op
+        self.label=label
+        self.grad=0.0
+        self._backward=lambda: None
+
+    def __repr__(self) -> str:
+        return f"Tensor(data={self.data})"
+
+    def __add__(self, other: type['Tensor']):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out=Tensor(data=self.data+other.data, _children=(self, other), _op='+')
+        def _backward():
+            self.grad+=out.grad
+            other.grad+=out.grad
+        out._backward=_backward
+
+    def __mul__(self, other: type['Tensor']):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out=Tensor(data=self.data*other.data, _children=(self, other), _op='*')
+        def _backward():
+            self.grad+=other.data*out.grad
+            other.grad+=self.data*out.grad
+        out._backward=_backward
+        return out
+
+    def __pow__(self, other: type['Tensor']):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out=Tensor(data=self.data**other.data, _children=(self, other), _op='**')
+        def _backward():
+            self.grad=(other.data*(self.data**(other.data-1)))*out.grad
+            other.grad=((np.log(self.data))*(self.data**other.data))*out.grad
+        out._backward=_backward
+        return out
+
+    def __neg__(self):
+        return self * (-1)
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __radd__(self, other):
+        return other+self
+
+    def __rmul__(self, other):
+        return other*self
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __rtruediv__(self, other):
+        return other * self**-1
+
+    def backwards(self):
+        topo=[]
+        visited=set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad=1.0
+        for node in reversed(topo):
+            node._backward()
