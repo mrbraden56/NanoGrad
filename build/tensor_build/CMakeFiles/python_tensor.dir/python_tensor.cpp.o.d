e74674e05715b45c41991494a59215c2c96c3673
Braden Lockwood <bradenlock83@gmail.com>
2023-03-16 07:01:05 -0500

    Successful pass of string and int from python to c++

---
+++ build/tensor_build/CMakeFiles/python_tensor.dir/python_tensor.cpp.o.d
@@ -0,0 +1,3 @@
+tensor_build/CMakeFiles/python_tensor.dir/python_tensor.cpp.o: \
+ /home/bradenlock83/Projects/NanoGrad/nano_grad/csrc/Tensor/python_tensor.cpp \
+ /usr/include/stdc-predef.h
--- nano_grad/csrc/Dispatcher/dispatcher.cpp
+++ nano_grad/csrc/Dispatcher/dispatcher.cpp
@@ -5,8 +5,19 @@ Dispatcher::Dispatcher(){
 }
 
 void Dispatcher::receive_python_object(PyObject* obj){
-    long value = PyLong_AsLong(obj);
-    std::cout << "Got Python Val: " << value << std::endl;
+    if (PyLong_Check(obj)) {
+        long value = PyLong_AsLong(obj);
+        if (value == -1 && PyErr_Occurred()) {
+            PyErr_Print();
+            return;
+        }
+        std::cout << "Got Python Int: " << value << std::endl;
+    } else if (PyUnicode_Check(obj)) {
+        const char* str = PyUnicode_AsUTF8(obj);
+        std::cout << "Got Python String: " << str << std::endl;
+    } else {
+        std::cout << "Unsupported Python object type" << std::endl;
+    }
 }
 
 extern "C" void receive_python_object_wrapper(PyObject* obj) {
